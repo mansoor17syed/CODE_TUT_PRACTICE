9da4356ee349ba44d3501aaa81a979e8c2586e3d
“mansoor17syed” <mansoor17syed@gmail.com>
2021-10-23 14:59:01 +0530

    JS DS and algo folder and files added

---
+++ JS_DS_ALGO/add_numbers.js
@@ -0,0 +1,14 @@
+console.log("add numbers")
+
+
+function addNumbers(n){
+    let total = 0
+    for(i=0;i<=n;i++){
+        total=total+i
+
+    }
+    return total
+
+}
+
+console.log(addNumbers(6))
---
+++ JS_PRACTICE_CWH/JS_interaction_alert_prom.js
@@ -0,0 +1,10 @@
+console.log("INTERCATION")
+
+alert("THIS IS ALLERT")
+
+let user_name=prompt("what is your NAME")
+
+console.log(user_name)
+
+let delete_post=confirm("Delet this post? SURE?")
+console.log(delete_post)
